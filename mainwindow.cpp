ce975751ab2bcc2da1a25949f61a58295c8a7184
Bhfdyz <1325667532@qq.com>
2025-12-30 09:36:59 +0800

    服务器

---
+++ mainwindow.cpp
@@ -0,0 +1,64 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTcpSocket>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->textEdit->setReadOnly(true);
+
+    _server = new QTcpServer(this);
+    int port = 8080;
+    QString hostport = "127.0.0.1";
+    if (!_server->listen(QHostAddress(hostport), port)) {
+        qDebug() << "连接失败";
+    }
+    connect(_server, &QTcpServer::newConnection, this, &MainWindow::SlotNewClient);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::SlotNewClient()
+{
+    if (_server->hasPendingConnections()) {
+        QTcpSocket *socket = _server->nextPendingConnection();
+        connect(socket, &QTcpSocket::readyRead,
+            this, &MainWindow::SlotReadFromClient);
+    }
+}
+
+void MainWindow::SlotReadFromClient()
+{
+    // 解析 json
+    QTcpSocket *tmpsocket = qobject_cast<QTcpSocket *>(sender());
+    QByteArray revData = tmpsocket->readAll();
+    QJsonDocument jsondoc = QJsonDocument::fromJson(revData);
+    QJsonObject jsonobj = jsondoc.object();
+    QString user = jsonobj["user"].toString();
+    QString password = jsonobj["password"].toString();
+    qDebug() << user;
+    qDebug() << password;
+
+    //  登录验证
+    if (user == "1234" && password == "1234") {
+        QList<QTcpSocket*> tcpSocketClients = _server->findChildren<QTcpSocket*>();
+        for (QTcpSocket* tmp : tcpSocketClients) {
+            tmp->write("yes");
+        }
+    } else {
+        QList<QTcpSocket*> tcpSocketClients = _server->findChildren<QTcpSocket*>();
+        for (QTcpSocket* tmp : tcpSocketClients) {
+            tmp->write("no");
+        }
+    }
+}
